940ec62f0fde5509bee1e2e237fc5adfb944d3b9
TisonCruz <tthoeu@uw.edu>
2023-04-06 23:27:52 -0700

    finished question 8

---
+++ question-8.sql
@@ -0,0 +1,10 @@
+--President Biden has issued an executive order that everyone who lives on a "Street" 
+--(as opposed to a "Trail", "Hill", "Drive" or other kind of address suffix) is actually 18 years old. Update the database accordingly.
+
+UPDATE persons
+set age = '18'
+where instr('Street', 'st');
+
+select address, age
+from persons
+where age = '18'
